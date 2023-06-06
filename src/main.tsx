80c5ba4e3e92eefa3ce231d69af4caf19f2748c7
unknown <omsup2544@gmail.com>
2023-06-06 13:55:09 +0700

    8th commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,15 @@ import "./index.css";
 import DetailCollection from "./DetailCollection.tsx";
 import RDTransaction from "./RDTransaction.tsx";
 import { InvoicePayment } from "./InvoicePayment.tsx";
+import RecieptAS9 from "./RecieptAS9.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
-    <DataResult />
+    {/* <DataResult /> */}
     {/* <DetailCollection /> */}
     {/* <RDTransaction /> */}
     {/* <InvoicePayment /> */}
+    <RecieptAS9/>
   </React.StrictMode>
 );
